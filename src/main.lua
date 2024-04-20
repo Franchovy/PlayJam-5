7a6ca0cc26dd05d04fafebc257e26a2796309b08
Maxime <maxrofra@gmail.com>
2024-04-20 03:34:53 +0200

    Updated main.lua

--- src/main.lua
+++ src/main.lua
@@ -3,6 +3,7 @@ import "libs"
 import "playdate"
 import "extensions"
 import "sprites"
+import "rooms"
 
 -- If your LDtk world is saved in multiple files (in this case you see a .ldtkl file for each level in your structure) you need to manually load the levels.
 -- LDtk.load_level( "TheFirstLevel" )
@@ -22,13 +23,25 @@ end
 --]]
 
 
-LDtk.load(assets.levels.test)
+-- Set up Scene Manager (Roomy)
 
-local levelName = "Level_1"
+local manager = Manager()
+manager:hook()
+
+-- Pre-load levels data
+
+LDtk.load(assets.levels.test)
 
 LDtk.loadAllLayersAsSprites(levelName)
 LDtk.loadAllEntitiesAsSprites(levelName)
 
+-- Open Menu
+--manager:enter(Menu())
+
 function playdate.update()
+    -- Update Scenes using Scene Manager
+    --manager:emit('update')
+
+    -- Update sprites
     playdate.graphics.sprite.update()
 end
