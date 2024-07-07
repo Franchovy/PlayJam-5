07bcc1241fc6840ef3d7ce94f7414167cd4f7557
CMorooney <coolercal@gmail.com>
2024-07-07 07:17:02 -0400

    fxp

--- src/rooms/game.lua
+++ src/rooms/game.lua
@@ -184,6 +184,8 @@ function Game:levelComplete(data)
     end
 
     local neighbors = LDtk.get_neighbours(currentLevelName, direction)
+   
+    if not neighbors then return end
 
     -- Check coordinates function for detecting which neighbor to transition to
     local nextLevel, nextLevelBounds = getNeighborLevelForPos(neighbors, coordinates)
