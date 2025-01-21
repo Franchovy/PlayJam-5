8486924ca4ffab891050a842924c3fa32f9ac9fb
Verynameboi <79213091+Verynameboi@users.noreply.github.com>
2025-01-21 14:07:22 -0600

    changes to tileset

--- src/sprites/Player.lua
+++ src/sprites/Player.lua
@@ -39,7 +39,7 @@ local ANIMATION_STATES = {
     Drilling = 4,
     Falling = 5,
     PreFalling = 6,
-    --Unsure = 7,
+    Unsure = 7
     --Land = 8
 }
 
@@ -93,7 +93,7 @@ function Player:init(entity)
     self:addState(ANIMATION_STATES.Drilling, 12, 16, { tickStep = 2 })
     self:addState(ANIMATION_STATES.Falling, 18, 20, { tickStep = 2 }) --thanks filigrani!
     self:addState(ANIMATION_STATES.PreFalling, 17, 17, { tickStep = 1, loopCount = 3 })
-    --self:addState(ANIMATION_STATES.Unsure, 24, 30, { tickStep = 2 }) --needs fix
+    self:addState(ANIMATION_STATES.Unsure, 24, 30, { tickStep = 2, loopCount = 1}) --needs fix
     --self:addState(ANIMATION_STATES.Impact, 21, 23, { tickStep = 2, loopCount = 3 }) --needs fix
 
     self:playAnimation()
@@ -723,7 +723,7 @@ function Player:isKeyPressedGated(key)
     if pd.buttonJustPressed(key) then
         self.questionMark:play()
         screenShake(3, 1)
-        --animationState = ANIMATION_STATES.Unsure
+        animationState = ANIMATION_STATES.Unsure
 
         spError:play(1)
     end
